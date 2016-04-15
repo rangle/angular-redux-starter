5f6858bf1f42bb1985447ccab8451bb56fc3f53a
Mark Hayes <mark.hayes@rangle.io>
2016-04-15 16:02:14 -0400

    Minor changes for visual consistency between ng1 and ng2 starters (using ng2 as guide)

--- src/components/navigator/navigator-item.ts
+++ src/components/navigator/navigator-item.ts
@@ -4,9 +4,16 @@ export class RioNavigatorItem {
 
   static options: ng.IComponentOptions = {
     transclude: true,
+    controllerAs: 'navigatorItemCtrl',
+    bindings: {
+      mr: '<',
+      ml: '<'
+    },
     template: `
-      <div class="mx1" ng-transclude>
-      </div>
+      <div class="truncate" 
+          ng-class="{'ml2': navigatorItemCtrl.ml, 'mr2': navigatorItemCtrl.mr}" 
+          ng-transclude>
+          </div>
     `
   };
 }
