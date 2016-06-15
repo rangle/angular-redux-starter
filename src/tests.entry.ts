dbffbed2c3ad8d60a00bb54f0f5426de0e2dd63c
Chris Bond <clbond@gmail.com>
2016-06-15 18:26:32 -0400

    Object.assign does not exist

--- src/tests.entry.ts
+++ src/tests.entry.ts
@@ -1,12 +1,3 @@
-const environment: any = window || this;
-Object.assign(environment, {
-  __DEV__: true,
-  __PRODUCTION__: false,
-  __TEST__: true
-});
-
-import './index.ts';
-
 const testContext = (<{ context?: Function }>require)
   .context('./', true, /\.test\.ts$/);
 
