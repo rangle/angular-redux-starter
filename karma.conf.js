3e9f7e05c6d68aca31c16ce08dd1c80d69e9fedd
Chris Bond <clbond@gmail.com>
2016-06-17 11:52:56 -0400

    Fix line-level code coverage report again
    Fix line-level code coverage report again
    
    implementing webpack copy plugin (#138)
    
    chore(package): update karma to version 1.0.0 (#143)
    
    https://greenkeeper.io/
    
    chore(package): update file-loader to version 0.9.0 (#141)
    
    https://greenkeeper.io/
    
    chore(package): update http-proxy to version 1.14.0 (#134)
    
    https://greenkeeper.io/
    
    chore(package): update postcss-modules-local-by-default to version 1.1.0 (#142)
    
    https://greenkeeper.io/
    
    chore(package): update tslint-loader to version 2.1.5 (#139)
    
    https://greenkeeper.io/
    
    Fixes Vendor.js not compiling (#144)
    
    * Fixes Vendor.js not compiling
    
    * Fix unit tests.
    
    Support running in dev mode inside of a Docker image by binding to all available network interfaces
    
    Fix line-level code coverage reports again
    
    Remove --host 0.0.0.0 and put it in a different PR

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,8 @@ const loaders = require('./webpack/loaders');
 const postcssInit = require('./webpack/postcss');
 
 module.exports = (config) => {
+  const coverage = config.singleRun ? ['coverage'] : [];
+
   config.set({
     frameworks: [
       'chai',
@@ -22,6 +24,7 @@ module.exports = (config) => {
       'karma-sourcemap-loader',
       'karma-webpack',
       'karma-coverage',
+      'karma-remap-istanbul',
       'karma-mocha-reporter',
       'karma-spec-reporter',
       'karma-chrome-launcher',
@@ -38,12 +41,11 @@ module.exports = (config) => {
     ],
 
     preprocessors: {
-      './src/**/*.ts': [
+      './src/tests.entry.ts': [
         'webpack',
         'sourcemap',
       ],
-      './src/**/!(*.test|tests.*).ts': [
-        'coverage',
+      './src/**/!(*.test).(ts|js)': [
         'sourcemap',
       ],
     },
@@ -57,9 +59,9 @@ module.exports = (config) => {
       },
       module: {
         loaders: combinedLoaders(),
-        postLoaders: [
-          loaders.istanbulInstrumenter,
-        ],
+        postLoaders: config.singleRun
+          ? [ loaders.istanbulInstrumenter ]
+          : [ ],
       },
       stats: { colors: true, reasons: true },
       debug: false,
@@ -71,8 +73,17 @@ module.exports = (config) => {
       noInfo: true, // prevent console spamming when running in Karma!
     },
 
-    reporters: ['mocha', 'coverage'],
-    // only output json report to be remapped by remap-istanbul
+    reporters: ['mocha']
+      .concat(coverage)
+      .concat(coverage.length > 0 ? ['karma-remap-istanbul'] : []),
+
+    remapIstanbulReporter: {
+      src: 'coverage/chrome/coverage-final.json',
+      reports: {
+        html: 'coverage',
+      },
+    },
+
     coverageReporter: {
       reporters: [
         { type: 'json' },
@@ -89,20 +100,20 @@ module.exports = (config) => {
     autoWatch: true,
     browsers: ['Chrome'], // Alternatively: 'PhantomJS'
     captureTimeout: 6000,
-    singleRun: true,
   });
 };
 
 function combinedLoaders() {
   return Object.keys(loaders).reduce(function reduce(aggregate, k) {
     switch (k) {
-    case 'tslint': // intolerably slow
+    case 'istanbulInstrumenter':
+    case 'tslint':
       return aggregate;
     case 'ts':
     case 'tsTest':
       return aggregate.concat([ // force inline source maps
         Object.assign(loaders[k],
-          { query: { babelOptions: { sourceMaps: 'inline' } } })]);
+          { query: { babelOptions: { sourceMaps: 'both' } } })]);
     default:
       return aggregate.concat([loaders[k]]);
     }
